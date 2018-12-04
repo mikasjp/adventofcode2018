3b7d054705cb4f55eb2eb8d24c9eee1756c7a727
Mikołaj Kamiński <mkaminski321@gmail.com>
2018-12-04 09:42:51 +0100

    Day 4 - solved part 1

--- 04/solution.py
+++ 04/solution.py
@@ -1,11 +1,14 @@
 import datetime
+from dateutil import rrule
+from collections import Counter
+
 data = [x[1:].split("] ") for x in open("data.txt").read().split("\n")]
 
 def epoch(x):
     x = x.split(" ")
     d = list(map(int,x[0].split("-")))
     t = list(map(int,x[1].split(":")))
-    return datetime.datetime(d[0], d[1], d[2], t[0], t[1]).timestamp()
+    return datetime.datetime(d[0], d[1], d[2], t[0], t[1])#.timestamp()
 
 data = [{"timestamp": epoch(x[0]), "cmd": x[1]} for x in data]
 data = sorted(data, key = lambda k: k["timestamp"])
@@ -14,9 +17,26 @@ data = sorted(data, key = lambda k: k["timestamp"])
 guards = {}
 pointer = None
 
-print(data[:20])
+for row in data:
+    if(row["cmd"][:5] == "Guard"):
+        pointer = int(row["cmd"].split(" ")[1][1:])
+    elif(row["cmd"] == "falls asleep"):
+        guards[pointer] = guards.get(pointer, []) + [[row["timestamp"]]] 
+    elif(row["cmd"] == "wakes up"):
+        guards[pointer][-1].append(row["timestamp"])
 
-#for row in data:
-#    if(row["cmd"][:5] == "Guard"):
-#        pointer = row["cmd"].split(" ")[1][1:]
-#    elif(row["cmd"] == "wakesup")
+naps = {}
+
+for g,n in guards.items():
+    naps[g] = sum([(x[-1]-x[0]).seconds//60 for x in n])
+
+sleepyhead = max(naps, key=naps.get)
+
+minutes = [[z.minute for z in rrule.rrule(rrule.MINUTELY, dtstart=x[0], until=x[-1])] for x in guards[sleepyhead]]
+
+all_minutes = []
+
+for x in minutes:
+    all_minutes += x
+
+print(Counter(all_minutes).most_common(1)[0][0] * sleepyhead)
